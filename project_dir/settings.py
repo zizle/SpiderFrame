0eabb651a7b0b848c3ab09a920916f111dfa4c3e
zizle <zizle_lin@163.com>
2018-08-02 16:23:37 +0800

    项目配置可以覆盖默认配置

---
+++ project_dir/settings.py
@@ -0,0 +1,4 @@
+# _*_ coding:utf-8 _*_
+
+# 修改默认日志文件名称
+DEFAULT_LOG_FILENAME = '日志.log'    # 默认日志文件名称
---
+++ scrapy_plus/conf/settings.py
@@ -0,0 +1,6 @@
+# _*_ coding:utf-8 _*_
+
+
+from .default_settings import *
+
+from settings import *
