25fc6a3b7bbc9fe04ede629fe5194eb9b77b44b3
zizle <zizle_lin@163.com>
2018-08-03 20:40:47 +0800

    增加爬虫

--- project_dir/spiders.py
+++ project_dir/spiders.py
@@ -2,6 +2,22 @@
 
 from scrapy_plus.core.spider import Spider
 
+from scrapy_plus.http.request import Request
+
 
 class BaiduSpider(Spider):
     start_urls = ['http://www.baidu.com', 'https://www.taobao.com']
+
+
+class DoubanSpider(Spider):
+    start_urls = []
+
+    def start_requests(self):
+        # 重写start_requests方法，返回多个请求
+        base_url = 'http://movie.douban.com/top250?start='
+        for i in range(0, 250, 25):
+            url = base_url + str(i)
+            yield Request(url)
+
+    def parse(self,response):
+        pass
